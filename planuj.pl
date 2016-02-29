3dbfc1847b587f31dbdeb3c9d4f3dab313b930aa
Paweł Wiśniewski <pawel.wis93@gmail.com>
2016-02-29 20:55:15 +0100

    trudność and czas sections added

--- planuj.pl
+++ planuj.pl
@@ -9,13 +9,43 @@ use Plan::Bib;
 print "Witaj w programie planuj wycieczkę!\n\n";
 
 while(1){
-	print "Podaj jaką masz kondycję:\n[1] - Słaba\n[2] - przeciętna\n[3] - dobra\n[h] - pomoc w wyborze\n";
+	print "\nPodaj jaką masz kondycję:\n[1] - Słaba\n[2] - Przeciętna\n[3] - Dobra\n[h] - Pomoc\n";
 	$kondycja = <>;
-	if ( Plan::Bib::sprawdz_odp(3, $kondycja) ){
+	if( Plan::Bib::sprawdz_odp(3, $kondycja) ) {
 		last;
 	}
 }
 
+while(1){
+	print "\nJaką trudność wycieczki planujesz:\n[1] - Lekki spacer (np. doliną)\n[2] - Lekka górska wycieczka\n";
+	
+	if( $kondycja >= 2 ) {	
+		print "[3] - Średnia górska wycieczka\n";
+	}
+	if( $kondycja >= 3 ) {	
+		print "[4] - Trudna górska wycieczka\n";
+	}
+	
+	print "[h] - Pomoc\n";
+	$trudnosc = <>;
+	if ( Plan::Bib::sprawdz_odp($kondycja + 1, $trudnosc) ){
+		last;
+	}
+}
+
+while(1){
+	print "\nIle czasu chcesz przeznaczyć:\n[1] - Do 4 godzin\n[2] - Do 6 godzin\n";
+	
+	if( $trudnosc >= 3 ) {	
+		print "[3] - Do 8 godzin\n[4] - Ostra wyrypa\n";
+		$trudnosc = 3;
+	}
+	print "[h] - Pomoc\n";
+	$czas = <>;
+	if ( Plan::Bib::sprawdz_odp($trudnosc+1,$czas) ){
+		last;
+	}
+}
 
 
 print "Works!\n";
