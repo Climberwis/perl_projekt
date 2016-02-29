0966547f3c783dc1eedbca694e595b08ea56c0da
Paweł Wiśniewski <pawel.wis93@gmail.com>
2016-02-29 19:31:06 +0100

    sub open_track added

--- planuj.pl
+++ planuj.pl
@@ -1,13 +1,20 @@
-#! /usr/bin/perl
+#!/usr/bin/perl
 #Paweł Wiśniewski PJS_gr1
-#chop, split, join, undef
-#./wyrazy.pl n1 n2 plik1 plik2 ...
-# wypisujemy wyrazy od niższego do wyższego
-# jak pliku nie ma to wyrzucam error (nie byłem w stanie otworzyć takiego pliku)
+
 sub sprawdz_liczba {
 my($zakres, $sprawdz) = @_;
 	if( !($sprawdz =~ m{^\d+$}o) || $sprawdz==0 || $sprawdz > $zakres){
 		print "Nie rozpoznano odpowiedzi!\n";
 }}
 
-foreach $wejscie (@ARGV)
+sub open_track {
+my($url) = @_;
+system("x-www-browser \"$url\"");
+}
+
+#foreach $wejscie (@ARGV)
+
+
+
+
+print "Works!\n";
